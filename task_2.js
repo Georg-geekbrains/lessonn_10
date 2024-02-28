6314e5213afd8d145e9bfa5eb997ca5e9565ef2e
mac <georghome1@gmail.com>
2024-02-28 21:29:13 +0300

    first commit

---
+++ task_2.js
@@ -0,0 +1,69 @@
+/*
+Задание 2
+Необходимо из объекта, который лежит в константе post вывести значения, к которым приписан комментарий, в консоль.
+
+const post = {
+author: "John", // вывести этот текст
+postId: 23,
+comments: [
+{
+userId: 10,
+userName: "Alex",
+text: "lorem ipsum",
+rating: {
+likes: 10,
+dislikes: 2, // вывести это число
+},
+},
+{
+userId: 5, // вывести это число
+userName: "Jane",
+text: "lorem ipsum 2", // вывести этот текст
+rating: {
+likes: 3,
+dislikes: 1,
+},
+},
+],
+};
+*/
+const post = {
+    author: "John", // вывести этот текст
+    postId: 23,
+    comments: [
+      {
+        userId: 10,
+        userName: "Alex",
+        text: "lorem ipsum",
+        rating: {
+          likes: 10,
+          dislikes: 2, // вывести это число
+        },
+      },
+      {
+        userId: 5, // вывести это число
+        userName: "Jane",
+        text: "lorem ipsum 2", // вывести этот текст
+        rating: {
+          likes: 3,
+          dislikes: 1,
+        },
+      },
+    ],
+  };
+  
+  
+  post.comments.forEach((comment) => {
+    
+    if (comment.userId) {
+      console.log(comment.userId);
+    }
+    
+    if (comment.text) {
+      console.log(comment.text);
+    }
+    
+    if (comment.rating && comment.rating.dislikes) {
+      console.log(comment.rating.dislikes);
+    }
+  });
